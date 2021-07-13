dadb43d4f026ffcf753179109d7eb51c839f3ffd
Hayashiis <83798337+Hayashiis@users.noreply.github.com>
2021-07-13 18:02:57 +0900

    Add files via upload

---
+++ 210708/2200104054/ga_report.py
@@ -0,0 +1,114 @@
+import random
+
+from deap import base
+from deap import creator
+from deap import tools
+
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
+
+toolbox = base.Toolbox()
+
+toolbox.register("attr_bool", random.randint, 0, 1)
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, 100)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+def evalOneMax(individual):
+    return sum(individual),
+
+def selectTwo(ind1, ind2):
+    ind1_1, ind1_2 = ind1.copy(), ind2.copy()
+    ind2_1, ind2_2 = ind1.copy(), ind2.copy()    
+    
+    size = min(len(ind1), len(ind2))
+    cxpoint1 = random.randint(1, size)
+    cxpoint2 = random.randint(1, size - 1)
+    if cxpoint2 >= cxpoint1:
+        cxpoint2 += 1
+    else:  # Swap the two cx points
+        cxpoint1, cxpoint2 = cxpoint2, cxpoint1
+
+    ind1_1[cxpoint1:], ind1_2[cxpoint1:] \
+        = ind2[cxpoint1:], ind1[cxpoint1:]
+    ind2_1[cxpoint1:cxpoint2], ind2_2[cxpoint1:cxpoint2] \
+        = ind2[cxpoint1:cxpoint2], ind1[cxpoint1:cxpoint2]
+
+    if sum(ind1_1)>sum(ind2_1):
+        ind1 = ind1_1
+    else:
+        ind1 = ind2_1
+    if sum(ind1_2)>sum(ind2_2):
+        ind2 = ind1_2
+    else:
+        ind2 = ind2_2
+        
+    return ind1, ind2
+
+
+toolbox.register("evaluate", evalOneMax)
+toolbox.register("mate", selectTwo)
+toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+toolbox.register("select", tools.selTournament, tournsize=3)
+
+
+def main():
+    random.seed(64)
+    
+    pop = toolbox.population(n=300)
+    CXPB, MUTPB, NGEN = 0.5, 0.2, 100
+    
+    print("Start of evolution")
+
+    fitnesses = list(map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = fit
+    
+    print("  Evaluated %i individuals" % len(pop))
+    
+    for g in range(NGEN):
+        print("-- Generation %i --" % g)
+        
+        offspring = toolbox.select(pop, len(pop))
+        offspring = list(map(toolbox.clone, offspring))
+
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+
+            if random.random() < CXPB:
+                toolbox.mate(child1, child2)
+                del child1.fitness.values
+                del child2.fitness.values
+
+        for mutant in offspring:
+
+            if random.random() < MUTPB:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+    
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+        
+        print("  Evaluated %i individuals" % len(invalid_ind))
+        
+        pop[:] = offspring
+        
+        fits = [ind.fitness.values[0] for ind in pop]
+        
+        length = len(pop)
+        mean = sum(fits) / length
+        sum2 = sum(x*x for x in fits)
+        std = abs(sum2 / length - mean**2)**0.5
+        
+        print("  Min %s" % min(fits))
+        print("  Max %s" % max(fits))
+        print("  Avg %s" % mean)
+        print("  Std %s" % std)
+    
+    print("-- End of (successful) evolution --")
+    
+    best_ind = tools.selBest(pop, 1)[0]
+    print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+
+if __name__ == "__main__":
+    main()
